daf5e975aeee6c9816bea091d4046280bd66bbb6
Chihiro Hasegawa <1114chihiro@gmail.com>
2023-01-15 16:30:15 -0500

    Initial commit

---
+++ ShoeStoreInventory-main/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.shoestoreinventory {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
+    requires lombok;
+    requires java.desktop;
+
+    opens com.example.shoestoreinventory to javafx.fxml;
+    exports com.example.shoestoreinventory;
+    exports com.example.shoestoreinventory.showTables.products;
+    opens com.example.shoestoreinventory.addProducts to javafx.fxml;
+}
